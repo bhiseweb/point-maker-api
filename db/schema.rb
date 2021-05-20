b8c7859a1929bb262fda7079d301413f69b93516
Leena <leena.web11@gmail.com>
2021-05-20 17:58:58 +0530

    Add schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_103906) do
+ActiveRecord::Schema.define(version: 2021_05_19_150909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_103906) do
 
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.bigint "resource_owner_id"
-    t.bigint "application_id", null: false
+    t.bigint "application_id"
     t.string "token", null: false
     t.string "refresh_token"
     t.integer "expires_in"
@@ -31,18 +31,6 @@ ActiveRecord::Schema.define(version: 2021_05_17_103906) do
     t.index ["refresh_token"], name: "index_oauth_access_tokens_on_refresh_token", unique: true
     t.index ["resource_owner_id"], name: "index_oauth_access_tokens_on_resource_owner_id"
     t.index ["token"], name: "index_oauth_access_tokens_on_token", unique: true
-  end
-
-  create_table "oauth_applications", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "uid", null: false
-    t.string "secret", null: false
-    t.text "redirect_uri"
-    t.string "scopes", default: "", null: false
-    t.boolean "confidential", default: true, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["uid"], name: "index_oauth_applications_on_uid", unique: true
   end
 
   create_table "points", force: :cascade do |t|
@@ -66,6 +54,5 @@ ActiveRecord::Schema.define(version: 2021_05_17_103906) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "oauth_access_tokens", "oauth_applications", column: "application_id"
   add_foreign_key "points", "users", column: "creator_id"
 end
